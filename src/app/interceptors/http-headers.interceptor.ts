e3d6720a4bb5bb16ceab18337b928a657aa39f2e
MonikaNovak <monivakova@gmail.com>
2021-09-06 11:01:57 +0200

    added game tabs component to the details page diplaying info, screenshots and trailers

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -22,17 +22,6 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
     }
 }
 
-// req = req.clone({
-//     setHeaders: {
-//         'x-rapidapi-key': '1b8799dc37254559b5bd2e696ce2a713',
-//         'x-rapidapi-host': 'https://api.rawg.io/api/games?key=1b8799dc37254559b5bd2e696ce2a713&dates=2019-09-01,2019-09-30&platforms=18,1,7',
-//     },
-//     setParams: {
-//         key: '1b8799dc37254559b5bd2e696ce2a713', //not sure if the keys should be the same
-//     }
-// });
-// return next.handle(req);
-
 
 
 
